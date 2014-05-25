6114785db2d9f36f2513c20fe9f0fccd6fc5ccc5
mandyGao <gaolixiao1233@126.com>
2014-05-25 23:38:00 +0800

    Add a widget about custom dialog
    A widget that slide screen from bottom of screen

---
+++ src/com/example/com/mindpin/sample/KCSlideFloatView.java
@@ -0,0 +1,125 @@
+package com.example.com.mindpin.sample;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.FrameLayout.LayoutParams;
+
+@SuppressLint("NewApi")
+public class KCSlideFloatView extends Dialog implements android.view.View.OnClickListener {
+	private ImageButton closeButton;
+	private FrameLayout frameLayout;
+	private View contentView;
+	private boolean isFullScreen = false;
+	private float percentHeight = 0.6f;
+	private float scaleValue = 0.9f;
+
+	public KCSlideFloatView(Context context) {
+		super(context, R.style.dialog);
+		
+	}
+
+    /*
+     * if set true the closeButton will gone.
+     */
+	public void setFullScreen (boolean isFullScreen) {
+		this.isFullScreen = isFullScreen;
+		
+		
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dialog_style);
+		
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+		getWindow().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		int screenWidth = displayMetrics.widthPixels;
+		int screenHeight = displayMetrics.heightPixels;
+		Log.v("mandy", "screenWidth: " + screenWidth + "screenHeight: " + screenHeight);
+		LinearLayout linearLayout = (LinearLayout)findViewById(R.id.rootLayout);
+		LayoutParams layoutParams = new LayoutParams(screenWidth, (int)(screenHeight *percentHeight));
+		
+		linearLayout.setLayoutParams(layoutParams); 
+		
+		closeButton = (ImageButton) findViewById(R.id.closeButton);
+		closeButton.setOnClickListener(this);
+		frameLayout = (FrameLayout) findViewById(R.id.content);
+		if (null != contentView) {
+		   frameLayout.addView(contentView);
+		}
+	}
+
+	// 浮动框组件滑入屏幕
+	public void open() {
+
+		Window window = getWindow();
+		// 设置显示动画
+		window.setWindowAnimations(R.style.main_menu_animstyle);
+		window.setGravity(Gravity.BOTTOM);
+		setCanceledOnTouchOutside(true);
+		show();
+		if (isFullScreen) {
+			closeButton.setVisibility(View.GONE);
+		}
+		
+		if(getOwnerActivity() != null) {
+			getOwnerActivity().getWindow().getDecorView().setScaleX(scaleValue);
+	        getOwnerActivity().getWindow().getDecorView().setScaleY(scaleValue);
+		}
+
+	}
+	@Override
+	public void dismiss() {
+		super.dismiss();
+		 if(getOwnerActivity() != null) {
+				getOwnerActivity().getWindow().getDecorView().setScaleX(1.0f);
+		        getOwnerActivity().getWindow().getDecorView().setScaleY(1.0f);
+			}
+	}
+
+	// 浮动框组件滑出屏幕
+	public void close() {
+		 dismiss();
+	}
+
+	// 设置浮动框组件要显示的内容
+	public void setView(View view) {
+		this.contentView = view;
+
+	}
+     /*
+      * the dialog will dismiss when the closebutton is clicked.
+      * @see android.view.View.OnClickListener#onClick(android.view.View)
+      */
+	@Override
+	public void onClick(View v) {
+	       dismiss();
+		
+	}
+    /*
+     * 设置浮动组件占屏幕的百分比
+     */
+	public void setPercentHeight(float percentHeight) {
+		this.percentHeight = percentHeight;
+	}
+    /*
+     * 设置屏幕缩小的倍数
+     */
+	public void setScaleValue(float scaleValue) {
+		this.scaleValue = scaleValue;
+	}
+	
+	
+
+}
---
+++ src/com/example/com/mindpin/sample/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.com.mindpin.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		/* set it to be full screen */
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_main);
+
+	}
+
+	public void onClick(View view) {
+
+		Log.v("mandy", "onclick.....");
+		KCSlideFloatView customDialog = new KCSlideFloatView(this);
+		// set owerActivty
+		customDialog.setOwnerActivity(this);
+		LayoutInflater inflater = getLayoutInflater();
+		View popupView = inflater.inflate(R.layout.popup_layout, null);
+		Button button = (Button) popupView.findViewById(R.id.testButton);
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT)
+						.show();
+
+			}
+		});
+		// customDialog.setFullScreen(true);
+		customDialog.setPercentHeight(0.6f);
+		customDialog.setScaleValue(0.9f);
+		customDialog.setView(popupView);
+		customDialog.open();
+
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
